616b11867c94af5d2586a2ec0cc3c728c37e5799
Ygor Torres <ygorftf@gmail.com>
2023-12-02 07:16:45 -0300

    first commit

---
+++ src/main/java/com/ygorftf/ecommerce/entities/Category.java
@@ -0,0 +1,5 @@
+package com.ygorftf.ecommerce.entities;
+
+public class Category {
+    
+}
---
+++ src/main/java/com/ygorftf/ecommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.ygorftf.ecommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
